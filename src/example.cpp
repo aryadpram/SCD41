28fcc42c8f91e9004afda231caff3f1358842b3b
aryadpram <aryadpram@gmail.com>
2024-06-12 21:01:22 +0700

    last testing before adding sd card

--- src/example.cpp
+++ src/example.cpp
@@ -1,52 +1,67 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <SensirionI2CScd4x.h>
-
-/* scl kuning
-scd puith
-*/
+#include "SensirionI2CScd4x.h"
 
 SensirionI2CScd4x scd4x;
+
+unsigned long previousMillis = 0;   // Store the last time the sensor data was published
+const long interval = 3000;       // Interval at which to publish data (5 minutes)
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
-  scd4x.begin(Wire);
- 
   uint16_t error;
-  
-  error = scd4x.stopPeriodicMeasurement();
-  error = scd4x.startPeriodicMeasurement();
 
-} 
+  scd4x.begin(Wire);
+
+  error = scd4x.stopPeriodicMeasurement();
+  if (error) {
+      Serial.print("Error trying to execute stopPeriodicMeasurement(): ");
+      Serial.println(error);
+  }
+
+  // Start Measurement
+  error = scd4x.startPeriodicMeasurement();
+  if (error) {
+      Serial.print("Error trying to execute startPeriodicMeasurement(): ");
+      Serial.println(error);
+  }
+
+}
 
 void loop() {
-  uint16_t error;
-  uint16_t co2 = 0;
-  float temperature = 0.0f;
-  float humidity = 0.0f;
-  float toff = 0.0f;
-  float t = 0.0f;
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
 
-  error = scd4x.readMeasurement(co2, temperature, humidity);
-  if (error){
-    Serial.print(error);
-    return;
+    uint16_t error;
+    uint16_t co2 = 0;
+    float temperature = 0.0f;
+    float humidity = 0.0f;
+    bool isDataReady = false;
+
+    error = scd4x.getDataReadyFlag(isDataReady);
+    if (error) {
+        Serial.print("Error trying to execute getDataReadyFlag(): ");
+        Serial.println(error);
+        return;
+    }
+    if (!isDataReady) {
+        return;
+    }
+
+    error = scd4x.readMeasurement(co2, temperature, humidity);
+    if (error) {
+        Serial.print("Error trying to execute readMeasurement(): ");
+        Serial.println(error);
+    } else {
+        Serial.print("Co2:");
+        Serial.print(co2);
+        Serial.print("\t");
+        Serial.print("Temperature:");
+        Serial.print(temperature);
+        Serial.print("\t");
+        Serial.print("Humidity:");
+        Serial.println(humidity);
+    }
   }
-  Serial.print("Co2:");
-  Serial.print(co2);
-  Serial.print("\t");
-  Serial.print("Temperature:");
-  Serial.print(temperature);
-  Serial.print("\t");
-  Serial.print("Humidity:");
-  Serial.println(humidity);
-
-  error = scd4x.getTemperatureOffset(t);
-  Serial.print (t);
-  
-  toff = -10;
-  error = scd4x.setTemperatureOffset(toff);
-
-  delay(5000);
 }
